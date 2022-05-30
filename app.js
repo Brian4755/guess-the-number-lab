aeeaf16c7e60a97c53b76a61c82b88ade34c3d3b
Brian4755 <brian.mccune1@yahoo.com>
2022-05-30 00:56:14 -0500

    move statements within object

--- app.js
+++ app.js
@@ -3,24 +3,45 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+    let guess
+    while (guess !== this.secretNum) {
+      this.prevGuesses.push(this.getGuess())
+      if (guess < this.secretNum) {
+        alert(`Your guess is too low Previous guesses: ${game.prevGuesses}`)
+      }
+      else if (guess > this.secretNum) {
+        alert(`Your guess is too high Previous guesses: ${game.prevGuesses}`)
+      }
+    }
+    return guess
+    },
+
+    getGuess() {
+      let guess
+      while (guess >= this.smallestNum && guess <= this.biggestNum) { 
+        parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+      }
+      return guess
+    }
 }
 
-prevGuesses = []
+game.play()
 
-function Guess(num) {
-  return prevGuesses.push(num)
-}
 
-let getGuess = 0
-while (getGuess !== game.secretNum) {
-  getGuess = parseInt(prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}`))
-if (getGuess !== game.secretNum) {
-  Guess(getGuess)
-  alert(`Your past guesses are ${prevGuesses}`)
-}}
 
-alert(`Congratulations! You have correctly guessed the number ${game.secretNum}`)
+// function Guess(num) {
+//   return prevGuesses.push(num)
+// }
+
+// let guess = 0
+// while (guess !== game.secretNum) {
+// if (guess !== game.secretNum) {
+//   Guess(guess)
+//   alert(`Your past guesses are ${prevGuesses}`)
+// }}
+
+// alert(`Congratulations! You have correctly guessed the number ${game.secretNum}`)
