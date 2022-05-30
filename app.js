37d7a06c3519b0afbfa03d7a8ab1343b6ffa2a65
Brian4755 <brian.mccune1@yahoo.com>
2022-05-30 14:55:20 -0500

    Lab complete

--- app.js
+++ app.js
@@ -4,44 +4,35 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+
   play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    let guess
-    while (guess !== this.secretNum) {
+    console.log('play invoked')
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    console.log(this.secretNum)
+    console.log(this.prevGuesses[this.prevGuesses.length -1], 'test')
+    while (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum) {
       this.prevGuesses.push(this.getGuess())
-      if (guess < this.secretNum) {
-        alert(`Your guess is too low Previous guesses: ${game.prevGuesses}`)
+      if (this.prevGuesses[this.prevGuesses.length -1] < this.secretNum) {
+        alert(`Your guess is too low. Previous guesses: ${game.prevGuesses}`)
+      } else if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum) {
+        alert(`Your guess is too high. Previous guesses: ${game.prevGuesses}`)
+      } else if (this.prevGuesses[this.prevGuesses.length -1] === this.secretNum) {
+        alert(`Congratulations you have guessed the correct number ${this.secretNum} in ${this.prevGuesses.length} guesses`)
+        return
       }
-      else if (guess > this.secretNum) {
-        alert(`Your guess is too high Previous guesses: ${game.prevGuesses}`)
+    }
+  },
+  getGuess: function() {
+    console.log('getGuess invoked')
+    let guess
+    while (!guess || guess < this.smallestNum || guess > this.biggestNum) {
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+      if (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
+        alert('number is not valid')
       }
     }
     return guess
-    },
-
-    getGuess() {
-      let guess
-      while (guess >= this.smallestNum && guess <= this.biggestNum) { 
-        parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
-      }
-      return guess
-    }
+  }
 }
 
 game.play()
-
-
-
-// function Guess(num) {
-//   return prevGuesses.push(num)
-// }
-
-// let guess = 0
-// while (guess !== game.secretNum) {
-// if (guess !== game.secretNum) {
-//   Guess(guess)
-//   alert(`Your past guesses are ${prevGuesses}`)
-// }}
-
-// alert(`Congratulations! You have correctly guessed the number ${game.secretNum}`)
